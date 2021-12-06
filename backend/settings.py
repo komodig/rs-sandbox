9ebbaab9161e67f74171798f2379d2bb1ac19f04
Lutz Ballaschke <l.ballaschke@tastybyte.de>
2021-12-07 00:48:29 +0100

    scribbled docker install script

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('RUN_DEV')
 
-ALLOWED_HOSTS = ['reeknersprook.de', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- rsapp/views.py
+++ rsapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You've got the info.")
+    return HttpResponse("Success! You made it.")
