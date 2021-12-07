7e5d104a6092d4cfb9c7f164feff2fd9f88ff69e
Lutz Ballaschke <l.ballaschke@tastybyte.de>
2021-12-07 01:09:03 +0100

    added download script for docker-compose

--- debian10-install-docker.sh
+++ debian10-install-docker.sh
@@ -16,3 +16,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# download docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
